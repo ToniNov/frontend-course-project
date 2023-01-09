5a07373153aab1312aee3051edd4bb90b3c624fd
ToniNov <antoni.novik@yandex.ru>
2023-01-09 10:12:06 +0300

    Commit test

--- src/api/applicationApi.ts
+++ src/api/applicationApi.ts
@@ -1,13 +1,10 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 import {RootState} from "../app/store";
 
-
-
-
 export const applicationApi = createApi({
     reducerPath: 'applicationApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: process.env.REACT_APP_BASE_SERVER_URL,
+        baseUrl: process.env.REACT_APP_BASE_SERVER_URL_RAIL,
         prepareHeaders: (headers, {getState}) => {
             const {token} = (getState() as RootState).app.userData;
             if (token) {
