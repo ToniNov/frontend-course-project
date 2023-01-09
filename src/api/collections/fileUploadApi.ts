d5eaf827d7b901c6b859fb2f2c104d48d50b5da9
ToniNov <antoni.novik@yandex.ru>
2023-01-09 10:13:14 +0300

    Commit test

--- src/api/collections/fileUploadApi.ts
+++ src/api/collections/fileUploadApi.ts
@@ -1,7 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const CLOUD_API_KEY="1c2e537122705b492ac47ea2c01b4aa5"
-
 export const fileUploadApi = createApi({
   reducerPath: 'fileUploadApi',
   baseQuery: fetchBaseQuery({
@@ -10,7 +8,7 @@ export const fileUploadApi = createApi({
   endpoints: builder => ({
     uploadImage: builder.mutation<any, FormData>({
       query: body => ({
-        url: `upload?expiration=600&key=${CLOUD_API_KEY}`,
+        url: `upload?expiration=600&key=${process.env.REACT_APP_CLOUD_API_KEY}`,
         method: 'POST',
         body,
       }),
